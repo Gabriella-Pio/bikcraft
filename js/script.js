eec9dda78a396bf48d6ca0dc0db94ed5339197fa
Gabriella Pio Correa <piocorreagabriella@gmail.com>
2025-10-29 09:54:03 -0300

    criada função no script para ativar os itens do orçamento

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
+// ativar itens do menu
 const links = document.querySelectorAll('.header-menu a');
 
 // exemplo de funcionamento do includes
-console.log("javascript".includes("java"));
+console.log('javascript'.includes('java'));
 
 function ativarLinks(link) {
   const url = location.href;
   const href = link.href;
-    
-    if (url.includes(href)) {
-      link.classList.add('ativo');
-    }
+
+  if (url.includes(href)) {
+    link.classList.add('ativo');
   }
-  
-  links.forEach(ativarLinks);
+}
+
+links.forEach(ativarLinks);
+
+// ativar itens do orçamento
+const parametros = new URLSearchParams(location.search); // search = propriedade de busca da URL
+
+function ativarProduto(parametro) {
+  console.log("param: " + parametro);
+  const elemento = document.getElementById(parametro);
+  console.log("elem: " + elemento);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
