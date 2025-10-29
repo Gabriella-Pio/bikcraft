e95003de5129790c9e1623af77d0f11a1a847fdb
Gabriella Pio Correa <piocorreagabriella@gmail.com>
2025-10-29 09:35:12 -0300

    iniciada funcionalidade de menu ativo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+const links = document.querySelectorAll('.header-menu a');
+
+// exemplo de funcionamento do includes
+console.log("javascript".includes("java"));
+
+function ativarLinks(link) {
+  const url = location.href;
+  const href = link.href;
+
+  // if(url === href) {
+    //   console.log(link);
+    // }
+    
+    if (url.includes(href)) {
+      console.log(link);
+    }
+  }
+  
+  links.forEach(ativarLinks);
